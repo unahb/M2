59d72253f2c968677ee6deb153beb2898052f308
unahb <garg.bhanu1@gmail.com>
2019-09-15 14:57:33 -0400

    Bhanu (#8)
    * did the string changes
    
    * Added name

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String ret;
+      for (Char c: input) {
+        ret  = (Char)(c + 1);
+      }
+      return ret;
     }
     
     /**
